73529fb358e5c4f8bcea5ed2a9afe7add1c26c74
The7Even <161558340+The7Even@users.noreply.github.com>
2024-09-24 14:49:00 +0900

    fixed

--- C Programming/4th week/practice file.c
+++ C Programming/4th week/practice file.c
@@ -9,36 +9,38 @@ int main(void)
 		int num1 = 0;
 		int num2 = 0;
 
-		printf("정수 두개를 입력하세요. \n>>>");
-		scanf_s("%d %d", &num1, &num2);
-
-		printf("---------------------------\n");
-		if (num1 == 0 || num2 == 0) { // 둘 중 하나라도 0인 경우 - 프로그램 종료
-			printf("program shutting down (inputed 0)\n");
+		while (1) {
+			printf("정수 두개를 입력하세요. \n>>>");
+			scanf_s("%d %d", &num1, &num2);
 			printf("---------------------------\n");
-		}
-		else if (num1 < num2) { // 작은수 / 큰수 입력시 대응 (정방향 출력)
-			for (int dan = num1; dan <= num2; dan++) {
+			if (num1 == 0 || num2 == 0) { // 둘 중 하나라도 0인 경우 - 프로그램 종료
+				printf("program shutting down (inputed 0)\n");
+				printf("---------------------------\n");
+				break;
+			}
+			else if (num1 < num2) { // 작은수 / 큰수 입력시 대응 (정방향 출력)
+				for (int dan = num1; dan <= num2; dan++) {
+					for (int time = 1; time <= 9; time++) {
+						printf("%d × %d = %d\n", dan, time, dan * time);
+					}
+					printf("---------------------------\n");
+				}
+			}
+			else if (num1 > num2) { // 큰수 / 작은수 입력시 대응 (역방향 출력)
+				for (int dan = num1; dan >= num2; dan--) {
+					for (int time = 1; time <= 9; time++) {
+						printf("%d × %d = %d\n", dan, time, dan * time);
+					}
+					printf("---------------------------\n");
+				}
+			}
+			else if (num1 == num2) { // 서로 같은수 입력시 대응
+				int dan = num1;
 				for (int time = 1; time <= 9; time++) {
 					printf("%d × %d = %d\n", dan, time, dan * time);
 				}
 				printf("---------------------------\n");
 			}
-		}
-		else if (num1 > num2) { // 큰수 / 작은수 입력시 대응 (역방향 출력)
-			for (int dan = num1; dan >= num2; dan--) {
-				for (int time = 1; time <= 9; time++) {
-					printf("%d × %d = %d\n", dan, time, dan * time);
-				}
-				printf("---------------------------\n");
-			}
-		}
-		else if (num1 == num2) { // 서로 같은수 입력시 대응
-			int dan = num1;
-			for (int time = 1; time <= 9; time++) {
-				printf("%d × %d = %d\n", dan, time, dan * time);
-			}
-			printf("---------------------------\n");
 		}
 	}
 	else if (sel == 2) { // 문제번호 2 >> * 피라미드 출력
