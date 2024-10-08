ddfe8cef98274109d250d6a5d39a6a72b4de5fcd
The7Even <161558340+The7Even@users.noreply.github.com>
2024-10-08 14:18:41 +0900

    Finished

--- C Programming/6th week/file1.c
+++ C Programming/6th week/file1.c
@@ -6,7 +6,7 @@ int main(void)
 	int first_x = 0; int first_y = 0;
 	int second_x = 0; int second_y = 0;
 
-	int mdis = 0; int sdis = 0;
+	double mdis = 0; double sdis = 0;
 
 	while (1)
 	{
@@ -16,10 +16,14 @@ int main(void)
 		printf("*** 두 번째 좌표 입력 : ");
 		scanf_s("%d %d", &second_x, &second_y);
 
+		if (first_x == 0 && second_x == 0 && first_y == 0 && second_y == 0)
+			break;
+
 		mdis = fabs((double)(first_x - second_x)) + fabs((double)(first_y - second_y));
 		sdis = sqrt(pow((double)(first_x - second_x), 2) + pow((double)(first_y - second_y), 2));
 
 		printf(">>> 맨해튼 거리 : %.2f\n", mdis);
 		printf(">>> 직선 거리   : %.2f\n ", sdis);
 	}
+	printf("* 프로그램을 종료합니다 *\n");
 }
