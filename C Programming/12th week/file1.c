3a6287e47485669ee58ca7dee0b9590604b908aa
The7Even <161558340+The7Even@users.noreply.github.com>
2024-11-19 14:19:27 +0900

    Create file1.c

---
+++ C Programming/12th week/file1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int StrLen(const char* str);
+
+int main(void)
+{
+	char str[100];
+	fgets(str, sizeof(str), stdin);
+
+	printf("입력받은 문자열의 길이 : %d\n",StrLen(str));
+	return 0;
+}
+
+int StrLen(const char* str)
+{
+	int len = 0;
+	for (int i = 0; i < 50; i++)
+	{
+		if (str[i] == '\0')
+			return len;
+		else
+			len++;
+	}
+}
