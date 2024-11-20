30dd0dfe7fc5755fa017711584e8887927d4d87f
The7Even <161558340+The7Even@users.noreply.github.com>
2024-11-20 16:33:24 +0900

    Update file2.c

--- C Programming/12th week/file2.c
+++ C Programming/12th week/file2.c
@@ -6,7 +6,7 @@ int BinToInt(const char* str);
 
 int main(void)
 {
-	char str[31];
+	char str[32];
 	while (1)
 	{
 		printf("2진수 입력 : ");
@@ -26,7 +26,7 @@ int BinToInt(const char* str)
 	int total = 0;
 	for (int i = 0; i < len; i++)
 	{
-		if (str[len - i -1] == 49)
+		if (str[len - i - 1] == '1')
 		{
 			total += pow(2, (double)(i));
 		}
